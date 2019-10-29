fac4ae9870f10fbae17d43fd389d7bdadc9fe4fa
Cory House <housecor@gmail.com>
2019-10-29 13:12:56 -0500

    Fix initialState

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,10 +1,11 @@
 import { LOAD_USERS_SUCCESS } from "../actions/actionTypes";
-import { users } from "./initialState";
+import initialState from "./initialState";
 
-export default function userReducer(state = users, action) {
+export default function userReducer(state = initialState.users, action) {
   switch (action.type) {
     case LOAD_USERS_SUCCESS:
       return action.users; // this will update the redux store
+    // add a case for handling DELETE_USER_SUCCESS
     default:
       return state;
   }
